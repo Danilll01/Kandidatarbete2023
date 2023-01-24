a3092aefcf223cf9ac6af9acca467fb1eb2cc0b4
Turbobus <59963570+Turbobus@users.noreply.github.com>
2023-01-24 14:22:57 +0100

    Addded temporary example class

---
+++ Assets/Example.cs
@@ -0,0 +1,29 @@
+using Mono.Cecil.Cil;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Example : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [SerializeField] private int varaibleTest;
+    [HideInInspector] public int helloOnce;
+    public int helloAgain;
+
+    private void Example1() { }
+
+    public void Example2() { }
+
+
+}
