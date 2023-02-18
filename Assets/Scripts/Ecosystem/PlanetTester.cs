f6e05942fcf3d155abfced24871df94611de5fc4
Daniel <perssondaniel01@gmail.com>
2023-02-18 16:56:32 +0100

    Added test script to test planets

---
+++ Assets/Scripts/Ecosystem/PlanetTester.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetTester : MonoBehaviour
+{
+    public Planet planet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Universe.InitializeRandomWithSeed();
+        planet.Initialize();
+    }
+}
