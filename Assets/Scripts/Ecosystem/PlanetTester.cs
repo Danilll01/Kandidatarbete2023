a1041d9c07532c59badb5448029e230aa1431a4a
Daniel <perssondaniel01@gmail.com>
2023-03-07 11:53:07 +0100

    Removed unused code

--- Assets/Scripts/Ecosystem/PlanetTester.cs
+++ Assets/Scripts/Ecosystem/PlanetTester.cs
@@ -7,7 +7,6 @@ public class PlanetTester : MonoBehaviour
     public Planet planet;
     public PillPlayerController player;
 
-    private bool show = true;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,16 +14,7 @@ public class PlanetTester : MonoBehaviour
         Universe.InitializeRandomWithSeed();
         planet.Initialize(player.transform, Universe.random.Next(), true);
         planet.SetUpPlanetValues();
-        planet.ShowCreatures(true);
         player.Initialize(planet.gameObject);
     }
 
-    void Update()
-    {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            planet.ShowCreatures(!show);
-            show = !show;
-        }
-    }
 }
