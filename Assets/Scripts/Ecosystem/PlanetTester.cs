61c964f7ed229f345eaccd250beb58cd2825b5de
isakswe <isak.gusten@telia.com>
2023-03-15 19:12:20 +0100

    Fix bad merge

--- Assets/Scripts/Ecosystem/PlanetTester.cs
+++ Assets/Scripts/Ecosystem/PlanetTester.cs
@@ -10,12 +10,10 @@ public class PlanetTester : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        player.Initialize(planet);
-
         Universe.InitializeRandomWithSeed();
         planet.Initialize(player.transform, Universe.random.Next(), true);
         planet.SetUpPlanetValues();
-        player.Initialize(planet.gameObject);
+        player.Initialize(planet);
     }
 
 }
