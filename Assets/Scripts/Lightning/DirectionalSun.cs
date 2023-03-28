9077d101e5ab16da1f5677fbeb86b25fb8cf06fe
Turbobus <59963570+Turbobus@users.noreply.github.com>
2023-03-29 00:03:44 +0200

    Fixed atmosphere update calling to mirror the players actual current planet

--- Assets/Scripts/Lightning/DirectionalSun.cs
+++ Assets/Scripts/Lightning/DirectionalSun.cs
@@ -9,20 +9,15 @@ public class DirectionalSun : MonoBehaviour
     [SerializeField] private Gradient skyGradient;  // Sky color gradient
 
     private Transform sun;                          // Sun to point light from
-    private Planet currentPlanet;
-    private AtmosphereHandler atmosphere;
-    private float planetRadius;
+    private PillPlayerController currentPlayer;
 
     /// <summary>
     /// Initializes the script to make directional light point from the sun object
     /// </summary>
-    /// <param name="sun">The sun object to point light from</param>
     public void Initialize()
     {
-        this.sun = Universe.sunPosition;
-        currentPlanet = player.GetComponent<PillPlayerController>().attractor;
-        atmosphere = currentPlanet.atmosphereHandler;
-        planetRadius = currentPlanet.radius;
+        sun = Universe.sunPosition;
+        currentPlayer = player.GetComponent<PillPlayerController>();
     }
 
     // Update is called once per frame
@@ -36,16 +31,16 @@ public class DirectionalSun : MonoBehaviour
         // This basically works by calculating the distance to the planet light edge and the shortest distance to the sun.
         // Then whatever the player distance falls in between these values that is whats is looked up in the gradient and set as the color
         // Therefore when the player is on the back half of the planet the ambient light will be low and the more near the sun the lighter the light will be
-        if (currentPlanet != null) {
-            atmosphere.UpdateAtmosphereAmbient(skyGradient);
+        if (currentPlayer.attractor != null) {
+            currentPlayer.attractor.atmosphereHandler.UpdateAtmosphereAmbient(skyGradient);
         }
     }
 
     // Updates the direction of the light
-    private void UpdateDirection() {
-        if (this.sun != null) {
-            Vector3 sunToPlayerVector = Vector3.Normalize(player.transform.position - sun.position);
-            transform.rotation = Quaternion.LookRotation(sunToPlayerVector);
-        }
+    private void UpdateDirection()
+    {
+        if (sun == null) return;
+        Vector3 sunToPlayerVector = Vector3.Normalize(player.transform.position - sun.position);
+        transform.rotation = Quaternion.LookRotation(sunToPlayerVector);
     }
 }
