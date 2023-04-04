ac9b26e421a29429c10a879fd19275635f20b848
Elin F <elin.forsberg@live.com>
2023-04-04 18:30:39 +0200

    Test function now takes planets rotation into account

--- Assets/TestRotation.cs
+++ Assets/TestRotation.cs
@@ -10,9 +10,8 @@ public class TestRotation : MonoBehaviour
     public Transform pointToTeleportSunTo;
     public Transform pointToTeleportPlanetTo;
     private Quaternion rotationBefore;
+    private Quaternion planetRotationBefore;
     private Vector3 directionBefore;
-    private Vector3 directionAfter;
-    private Vector3 rotationAfter;
     private Vector3 directionBY;
 
 
@@ -20,7 +19,8 @@ public class TestRotation : MonoBehaviour
     void Start()
     {
         rotationBefore = Sun.transform.rotation;
-        directionBefore = Sun.transform.position - PlayerPlanet.transform.position;
+        planetRotationBefore = PlayerPlanet.transform.rotation;
+
         Vector3 sunUpBefore = Sun.transform.up;
 
         float heightDiff = Sun.transform.position.y - PlayerPlanet.transform.position.y;
@@ -29,17 +29,16 @@ public class TestRotation : MonoBehaviour
 
         directionBY = directionBefore;
         directionBY.y = 0;
+        directionBY = PlayerPlanet.transform.rotation * directionBY;
 
         Sun.transform.rotation = Quaternion.identity;
         Sun.transform.position = pointToTeleportSunTo.position;
         PlayerPlanet.transform.position = pointToTeleportPlanetTo.position;
 
-        Vector3 resetVector = sunUpBefore;
-        directionAfter = resetVector - PlayerPlanet.transform.position;
-
         Vector3 newVector = Sun.transform.position + sunUpBefore * heightDiff - PlayerPlanet.transform.position;
         PlayerPlanet.transform.rotation *= Quaternion.FromToRotation(directionBY, newVector);
         PlayerPlanet.transform.rotation *= Quaternion.Inverse(rotationBefore);
+        PlayerPlanet.transform.rotation *= planetRotationBefore;
 
     }
 
@@ -53,15 +52,17 @@ public class TestRotation : MonoBehaviour
         directionBY = directionBefore;
         directionBY.y = 0;
 
-        Vector3 resetVector = sunUpBefore;
-        directionAfter = resetVector - PlayerPlanet.transform.position;
-
         Gizmos.color = Color.blue;
+        Gizmos.DrawRay(PlayerPlanet.transform.position, directionBY);
+
+        Gizmos.color = Color.black;
+        directionBY  = PlayerPlanet.transform.rotation * directionBY;
         Gizmos.DrawRay(PlayerPlanet.transform.position, directionBY);
 
 
         Gizmos.color = Color.red;
-        Gizmos.DrawRay(Sun.transform.position, Sun.transform.up * heightDiff);
+        sunUpBefore = PlayerPlanet.transform.rotation * sunUpBefore;
+        Gizmos.DrawRay(Sun.transform.position, sunUpBefore * heightDiff);
 
         Gizmos.color = Color.green;
         Vector3 newVector = Sun.transform.position + Sun.transform.up * heightDiff - PlayerPlanet.transform.position;
