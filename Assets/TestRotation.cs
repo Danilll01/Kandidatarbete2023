583a4f97673ae09d21c016995a0c9a80dad5188d
Elin F <elin.forsberg@live.com>
2023-04-04 14:26:54 +0200

    Progress

--- Assets/TestRotation.cs
+++ Assets/TestRotation.cs
@@ -13,6 +13,7 @@ public class TestRotation : MonoBehaviour
     private Vector3 directionBefore;
     private Vector3 directionAfter;
     private Vector3 rotationAfter;
+    private Vector3 directionBY;
 
 
     // Start is called before the first frame update
@@ -20,36 +21,48 @@ public class TestRotation : MonoBehaviour
     {
         rotationBefore = Sun.transform.rotation;
         directionBefore = Sun.transform.position - PlayerPlanet.transform.position;
+        Vector3 sunUpBefore = Sun.transform.up;
 
-        // Sun.transform.position = pointToTeleportSunTo.transform.position;
-        //PlayerOrbitObject.transform.position = pointToTeleportPlanetTo.transform.position;
+        float heightDiff = Sun.transform.position.y - PlayerPlanet.transform.position.y;
+
+        directionBefore = Sun.transform.position - PlayerPlanet.transform.position;
+
+        directionBY = directionBefore;
+        directionBY.y = 0;
 
         Sun.transform.rotation = Quaternion.identity;
 
-        rotationAfter = Sun.transform.rotation.eulerAngles;
+        Vector3 resetVector = sunUpBefore;
+        directionAfter = resetVector - PlayerPlanet.transform.position;
 
-        directionAfter = Sun.transform.position - PlayerPlanet.transform.position;
-
+        Vector3 newVector = Sun.transform.position + sunUpBefore * heightDiff - PlayerPlanet.transform.position;
+        PlayerPlanet.transform.rotation *= Quaternion.FromToRotation(directionBY, newVector);
         PlayerPlanet.transform.rotation *= Quaternion.Inverse(rotationBefore);
-        //PlayerPlanet.transform.rotation *= Quaternion.FromToRotation(directionBefore, directionAfter); ;
 
     }
 
     private void OnDrawGizmos()
     {
-        
+        float heightDiff = Sun.transform.position.y - PlayerPlanet.transform.position.y;
+
+        directionBefore = Sun.transform.position - PlayerPlanet.transform.position;
+        Vector3 sunUpBefore = Sun.transform.up;
+
+        directionBY = directionBefore;
+        directionBY.y = 0;
+
+        Vector3 resetVector = sunUpBefore;
+        directionAfter = resetVector - PlayerPlanet.transform.position;
+
+        Gizmos.color = Color.blue;
+        Gizmos.DrawRay(PlayerPlanet.transform.position, directionBY);
+
 
         Gizmos.color = Color.red;
+        Gizmos.DrawRay(Sun.transform.position, Sun.transform.up * heightDiff);
 
-
-        if (directionBefore != null && directionAfter != null)
-        {
-            Gizmos.color = Color.blue;
-            Gizmos.DrawRay(PlayerPlanet.transform.position, directionBefore);
-
-            Gizmos.color = Color.red;
-            Gizmos.DrawRay(PlayerPlanet.transform.position, directionAfter);
-
-        }
+        Gizmos.color = Color.green;
+        Vector3 newVector = Sun.transform.position + Sun.transform.up * heightDiff - PlayerPlanet.transform.position;
+        Gizmos.DrawRay(PlayerPlanet.transform.position, newVector);
     }
 }
