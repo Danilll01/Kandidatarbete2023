b3c53cda899bd96d7074f2b780cf0476101f8935
Elin F <elin.forsberg@live.com>
2023-04-06 12:59:41 +0200

    Setup test scene for planets position after reset

--- Assets/TestRotation.cs
+++ Assets/TestRotation.cs
@@ -4,68 +4,38 @@ using UnityEngine;
 
 public class TestRotation : MonoBehaviour
 {
-    public GameObject Sun;
-    public GameObject PlayerPlanet;
-    public GameObject PlayerOrbitObject;
+    public GameObject sun;
+    public GameObject planet;
     public Transform pointToTeleportSunTo;
-    public Transform pointToTeleportPlanetTo;
-    private Quaternion rotationBefore;
-    private Quaternion planetRotationBefore;
-    private Vector3 directionBefore;
-    private Vector3 directionBY;
+    private Vector3 startDirection;
+    private Vector3 newDirection;
+    private Quaternion originalSunRotation;
 
 
     // Start is called before the first frame update
     void Start()
     {
-        rotationBefore = Sun.transform.rotation;
-        planetRotationBefore = PlayerPlanet.transform.rotation;
+        originalSunRotation = sun.transform.rotation;
+        startDirection = planet.transform.position - sun.transform.position;
+        sun.transform.position = pointToTeleportSunTo.position;
+        sun.transform.rotation = Quaternion.identity;
+        newDirection = Quaternion.Inverse(originalSunRotation) * startDirection;
+        planet.transform.rotation = Quaternion.identity;
+        planet.transform.position = sun.transform.position + newDirection;
+        
 
-        Vector3 sunUpBefore = Sun.transform.up;
 
-        float heightDiff = Sun.transform.position.y - PlayerPlanet.transform.position.y;
-
-        directionBefore = Sun.transform.position - PlayerPlanet.transform.position;
-
-        directionBY = directionBefore;
-        directionBY.y = 0;
-        directionBY = PlayerPlanet.transform.rotation * directionBY;
-
-        Sun.transform.rotation = Quaternion.identity;
-        Sun.transform.position = pointToTeleportSunTo.position;
-        PlayerPlanet.transform.position = pointToTeleportPlanetTo.position;
-
-        Vector3 newVector = Sun.transform.position + sunUpBefore * heightDiff - PlayerPlanet.transform.position;
-        PlayerPlanet.transform.rotation *= Quaternion.FromToRotation(directionBY, newVector);
-        PlayerPlanet.transform.rotation *= Quaternion.Inverse(rotationBefore);
-        PlayerPlanet.transform.rotation *= planetRotationBefore;
 
     }
 
     private void OnDrawGizmos()
     {
-        float heightDiff = Sun.transform.position.y - PlayerPlanet.transform.position.y;
 
-        directionBefore = Sun.transform.position - PlayerPlanet.transform.position;
-        Vector3 sunUpBefore = Sun.transform.up;
-
-        directionBY = directionBefore;
-        directionBY.y = 0;
-
-        Gizmos.color = Color.blue;
-        Gizmos.DrawRay(PlayerPlanet.transform.position, directionBY);
-
-        Gizmos.color = Color.black;
-        directionBY  = PlayerPlanet.transform.rotation * directionBY;
-        Gizmos.DrawRay(PlayerPlanet.transform.position, directionBY);
-
-
-        Gizmos.color = Color.red;
-        sunUpBefore = PlayerPlanet.transform.rotation * sunUpBefore;
-        Gizmos.DrawRay(Sun.transform.position, sunUpBefore * heightDiff);
-
-        Gizmos.color = Color.green;
-        Vector3 newVector = Sun.transform.position + sunUpBefore * heightDiff - PlayerPlanet.transform.position;
-        Gizmos.DrawRay(PlayerPlanet.transform.position, newVector);
+        if (newDirection != null)
+        {
+            Gizmos.color = Color.blue;
+            Gizmos.DrawLine(sun.transform.position, sun.transform.position + newDirection);
+        }
+        
     }
 }
