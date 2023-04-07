5992bcec76cf0482aff49d181ed8f528e0a6d6bd
Turbobus <59963570+Turbobus@users.noreply.github.com>
2023-04-07 02:24:31 +0200

    Added new spaceship controller

---
+++ Assets/Scripts/MovementControllers/SpaceShipController.cs
@@ -0,0 +1,88 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class SpaceShipController : MonoBehaviour
+{
+    [SerializeField] private float normalSpeed = 25f;
+    [SerializeField] private float accelerationSpeed = 45f;
+    [SerializeField] private Transform cameraPosition;
+    [SerializeField] private Camera mainCamera;
+    [SerializeField] private Transform spaceshipRoot;
+    [SerializeField] private float rotationSpeed = 2.0f;
+    [SerializeField] private float cameraSmooth = 4f;
+    [SerializeField] private RectTransform crosshairTexture;
+
+    private float speed;
+    private Rigidbody physicsBody;
+    private Quaternion lookRotation;
+    private float rotationZ = 0;
+    private float mouseXSmooth = 0;
+    private float mouseYSmooth = 0;
+    private Vector3 defaultShipRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        physicsBody = GetComponent<Rigidbody>();
+        physicsBody.useGravity = false;
+        lookRotation = transform.rotation;
+        defaultShipRotation = spaceshipRoot.localEulerAngles;
+        rotationZ = defaultShipRotation.z;
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void FixedUpdate()
+    {
+        //Press Right Mouse Button to accelerate
+        if (Input.GetMouseButton(1))
+        {
+            speed = Mathf.Lerp(speed, accelerationSpeed, Time.deltaTime * 3);
+        }
+        else
+        {
+            speed = Mathf.Lerp(speed, normalSpeed, Time.deltaTime * 10);
+        }
+
+        //Set moveDirection to the vertical axis (up and down keys) * speed
+        Vector3 moveDirection = new Vector3(0, 0, speed);
+        //Transform the vector3 to local space
+        moveDirection = transform.TransformDirection(moveDirection);
+        //Set the velocity, so you can move
+        physicsBody.velocity = new Vector3(moveDirection.x, moveDirection.y, moveDirection.z);
+
+        //Camera follow
+        mainCamera.transform.position = Vector3.Lerp(mainCamera.transform.position, cameraPosition.position, Time.deltaTime * cameraSmooth);
+        mainCamera.transform.rotation = Quaternion.Lerp(mainCamera.transform.rotation, cameraPosition.rotation, Time.deltaTime * cameraSmooth);
+
+        //Rotation
+        float rotationZTmp = 0;
+        if (Input.GetKey(KeyCode.A))
+        {
+            rotationZTmp = 1;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            rotationZTmp = -1;
+        }
+        mouseXSmooth = Mathf.Lerp(mouseXSmooth, Input.GetAxis("Horizontal Look") * rotationSpeed, Time.deltaTime * cameraSmooth);
+        mouseYSmooth = Mathf.Lerp(mouseYSmooth, Input.GetAxis("Vertical Look") * rotationSpeed, Time.deltaTime * cameraSmooth);
+        Quaternion localRotation = Quaternion.Euler(-mouseYSmooth, mouseXSmooth, rotationZTmp * rotationSpeed);
+        lookRotation = lookRotation * localRotation;
+        transform.rotation = lookRotation;
+        rotationZ -= mouseXSmooth;
+        rotationZ = Mathf.Clamp(rotationZ, -45, 45);
+        spaceshipRoot.transform.localEulerAngles = new Vector3(defaultShipRotation.x, defaultShipRotation.y, rotationZ);
+        rotationZ = Mathf.Lerp(rotationZ, defaultShipRotation.z, Time.deltaTime * cameraSmooth);
+
+        //Update crosshair texture
+        if (crosshairTexture)
+        {
+            crosshairTexture.position = mainCamera.WorldToScreenPoint(transform.position + transform.forward * 100);
+        }
+    }
+}
