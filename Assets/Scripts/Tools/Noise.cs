86d657bb7fbf264f928ad4b65f161cc7692d5602
Manfred Hästmark <hastmark@student.chalmers.se>
2023-04-14 14:14:51 +0200

    Added 3d worley noise

--- Assets/Scripts/Tools/Noise.cs
+++ Assets/Scripts/Tools/Noise.cs
@@ -365,6 +365,118 @@ namespace Noise
         /// <summary>
         /// Evaluate cells at position
         /// </summary>
+        public static float EvaluateCells(float x, float y, float z, float angleOffset)
+        {
+            return EvaluateCells(new float3(x, y, z), angleOffset);
+        }
+
+        /// <summary>
+        /// Evaluate cells at position
+        /// </summary>
+        public static float EvaluateCells(Vector3 pos, float angleOffset)
+        {
+            return EvaluateCells(new float3(pos.x, pos.y, pos.z), angleOffset);
+        }
+
+        /// <summary>
+        /// Evaluate cells at position
+        /// </summary>
+        public static float EvaluateCells(float3 pos, float angleOffset)
+        {
+            float X = Mathf.Floor(pos.x);
+            float Y = Mathf.Floor(pos.y);
+            float Z = MathF.Floor(pos.z);
+            float3 f = pos - new float3(X, Y, Z);
+            float2 res = new float2(8.0f, 0);
+
+            for (int z = -1; z <= 1; z++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    for (int x = -1; x <= 1; x++)
+                    {
+                        float3 lattice = new float3(x, y, z);
+                        float3 offset = Details.randomVector(lattice + new float3(X, Y, Z), angleOffset);
+                        Vector3 diff = lattice + offset - f;
+                        float d = diff.magnitude;
+
+                        if (d < res.x)
+                        {
+                            res = new float2(d, offset.x);
+                        }
+                    }
+                }
+            }
+            return res.y;
+        }
+
+        /// <summary>
+        /// Evaluate noise at position
+        /// </summary>
+        public static float EvaluateNoise(float x, float y, float z, float angleOffset)
+        {
+            return EvaluateNoise(new float3(x, y, z), angleOffset);
+        }
+
+        /// <summary>
+        /// Evaluate noise at position
+        /// </summary>
+        public static float EvaluateNoise(Vector3 pos, float angleOffset)
+        {
+            return EvaluateNoise(new float3(pos.x, pos.y, pos.z), angleOffset);
+        }
+
+        /// <summary>
+        /// Evaluate noise at position
+        /// </summary>
+        public static float EvaluateNoise(float3 pos, float angleOffset)
+        {
+            float X = Mathf.Floor(pos.x);
+            float Y = Mathf.Floor(pos.y);
+            float Z = MathF.Floor(pos.z);
+            float3 f = pos - new float3(X, Y, Z);
+            float res = 8.0f;
+
+            for (int z = -1; z <= 1; z++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    for (int x = -1; x <= 1; x++)
+                    {
+                        float3 lattice = new float3(x, y, z);
+                        float3 offset = Details.randomVector(lattice + new float3(X, Y, Z), angleOffset);
+                        Vector3 diff = lattice + offset - f;
+                        float d = diff.magnitude;
+
+                        if (d < res)
+                        {
+                            res = d;
+                        }
+                    }
+                }
+            }
+            return res;
+        }
+
+        /// <summary>
+        /// Evaluate cells at position
+        /// </summary>
+        public static float EvaluateCells(float x, float y, float angleOffset)
+        {
+            return EvaluateCells(new float2(x, y), angleOffset);
+        }
+
+        /// <summary>
+        /// Evaluate cells at position
+        /// </summary>
+        public static float EvaluateCells(Vector2 pos, float angleOffset)
+        {
+            return EvaluateCells(new float2(pos.x, pos.y), angleOffset);
+        }
+
+        /// <summary>
+        /// Evaluate cells at position
+        /// </summary>
         public static float EvaluateCells(float2 pos, float angleOffset)
         {
             float X = Mathf.Floor(pos.x);
@@ -388,6 +500,22 @@ namespace Noise
                 }
             }
             return res.y;
+        }
+
+        /// <summary>
+        /// Evaluate noise at position
+        /// </summary>
+        public static float EvaluateNoise(float x, float y, float angleOffset)
+        {
+            return EvaluateNoise(new float2(x, y), angleOffset);
+        }
+
+        /// <summary>
+        /// Evaluate noise at position
+        /// </summary>
+        public static float EvaluateNoise(Vector2 pos, float angleOffset)
+        {
+            return EvaluateNoise(new float2(pos.x, pos.y), angleOffset);
         }
 
         /// <summary>
@@ -506,12 +634,28 @@ namespace Noise
         }
 
         /// <summary>
+        /// Returns a random vector
+        /// </summary>
+        public static Vector3 randomVector(Vector3 UV, float offset)
+        {
+            Matrix<double> m = DenseMatrix.OfArray(new double[,] {
+                { 52.61, 24.96, 66.04 },
+                {  4.22, 70.89, 26.07 },
+                { 46.86, 40.52, 21.76 } });
+
+            Matrix<double> uv = DenseMatrix.OfArray(new double[,] { { UV.x, UV.y, UV.z } });
+
+            UV.x = Mathf.Sin((float)(uv * m)[0, 0]) % 1;
+            UV.y = Mathf.Sin((float)(uv * m)[0, 1]) % 1;
+            UV.z = Mathf.Sin((float)(uv * m)[0, 2]) % 1;
+
+            return new Vector3(Mathf.Sin(UV.z * +offset) * 0.5f + 0.5f, Mathf.Cos(UV.x * offset) * 0.5f + 0.5f, Mathf.Sin(UV.y * +offset) * 0.5f + 0.5f);
+        }
+
+        /// <summary>
         /// Integer array used to retrieve pseudo-random hash values
         /// </summary>
         public static int[] p =
         { 151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180, 151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180 };
-
     }
 }
-
-
