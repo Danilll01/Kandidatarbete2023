4c789964fced6cbbb36ee0a8ef9d1306ae65493b
Manfred Hästmark <hastmark@student.chalmers.se>
2023-04-14 12:49:15 +0200

    Shadergraph and HLSL version done

--- Assets/Shaders/Tools/Noise/Noise.hlsl
+++ Assets/Shaders/Tools/Noise/Noise.hlsl
@@ -8,6 +8,8 @@ float3 fade(float3 t);
 float grad(int hash, float x);
 float grad(int hash, float x, float y);
 float grad(int hash, float x, float y, float z);
+float2 randomVector(float2 UV, float offset);
+float3 randomVector(float3 UV, float offset);
 
 static const int p[512] =
 { 151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180, 151, 160, 137, 91, 90, 15, 131, 13, 201, 95, 96, 53, 194, 233, 7, 225, 140, 36, 103, 30, 69, 142, 8, 99, 37, 240, 21, 10, 23, 190, 6, 148, 247, 120, 234, 75, 0, 26, 197, 62, 94, 252, 219, 203, 117, 35, 11, 32, 57, 177, 33, 88, 237, 149, 56, 87, 174, 20, 125, 136, 171, 168, 68, 175, 74, 165, 71, 134, 139, 48, 27, 166, 77, 146, 158, 231, 83, 111, 229, 122, 60, 211, 133, 230, 220, 105, 92, 41, 55, 46, 245, 40, 244, 102, 143, 54, 65, 25, 63, 161, 1, 216, 80, 73, 209, 76, 132, 187, 208, 89, 18, 169, 200, 196, 135, 130, 116, 188, 159, 86, 164, 100, 109, 198, 173, 186, 3, 64, 52, 217, 226, 250, 124, 123, 5, 202, 38, 147, 118, 126, 255, 82, 85, 212, 207, 206, 59, 227, 47, 16, 58, 17, 182, 189, 28, 42, 223, 183, 170, 213, 119, 248, 152, 2, 44, 154, 163, 70, 221, 153, 101, 155, 167, 43, 172, 9, 129, 22, 39, 253, 19, 98, 108, 110, 79, 113, 224, 232, 178, 185, 112, 104, 218, 246, 97, 228, 251, 34, 242, 193, 238, 210, 144, 12, 191, 179, 162, 241, 81, 51, 145, 235, 249, 14, 239, 107, 49, 192, 214, 31, 181, 199, 106, 157, 184, 84, 204, 176, 115, 121, 50, 45, 127, 4, 150, 254, 138, 236, 205, 93, 222, 114, 67, 29, 24, 72, 243, 141, 128, 195, 78, 66, 215, 61, 156, 180 };
@@ -375,6 +377,83 @@ void EvaluateSimplex_float(float2 UV, float Scale, out float Out)
     Out = simplex.Evaluate(UV * Scale);
 }
 
+class Worley
+{
+    // Function declarations
+    void Evaluate(float3 pos, float angleOffset, out float Out, out float Cells);
+    void Evaluate(float2 pos, float angleOffset, out float Out, out float Cells);
+    
+    // Evaluate worley noise
+    void Evaluate(float3 pos, float angleOffset, out float Out, out float Cells)
+    {
+        float3 g = floor(pos);
+        float3 f = frac(pos);
+        float t = 8.0;
+        float4 res = float4(t, 0, 0, 0);
+
+        for (int z = -1; z <= 1; z++)
+        {
+            for (int y = -1; y <= 1; y++)
+            {
+                for (int x = -1; x <= 1; x++)
+                {
+                    float3 lattice = float3(x, y, z);
+                    float3 offset = randomVector(lattice + g, angleOffset);
+                    float d = distance(lattice + offset, f);
+                    
+                    if (d < res.x)
+                    {
+                        res = float4(d, offset.x, offset.y, offset.z);
+                        Out = res.x;
+                        Cells = res.y;
+                    }
+                }
+            }
+        }
+    }
+    
+    
+    // Evaluate worley noise
+    void Evaluate(float2 pos, float angleOffset, out float Out, out float Cells)
+    {
+        float2 g = floor(pos);
+        float2 f = frac(pos);
+        float t = 8.0;
+        float3 res = float3(8.0, 0.0, 0.0);
+        
+        for (int y = -1; y <= 1; y++)
+        {
+            for (int x = -1; x <= 1; x++)
+            {
+                float2 lattice = float2(x, y);
+                float2 offset = randomVector(lattice + g, angleOffset);
+                float d = distance(lattice + offset, f);
+
+                if (d < res.x)
+                {
+                    res = float3(d, offset.x, offset.y);
+                    Out = res.x;
+                    Cells = res.y;
+                }
+            }
+        }
+    }
+};
+
+Worley worley;
+
+// Evaluate at point UV using Scale
+void EvaluateWorley_float(float3 UV, float AngleOffset, float CellDensity, out float Out, out float Cells)
+{
+    worley.Evaluate(UV * CellDensity, AngleOffset, Out, Cells);
+}
+
+// Evaluate at point UV using Scale
+void EvaluateWorley_float(float2 UV, float AngleOffset, float CellDensity, out float Out, out float Cells)
+{
+    worley.Evaluate(UV * CellDensity, AngleOffset, Out, Cells);
+}
+
 // Helper functions
 float lerp(float a, float b, float t)
 {
@@ -414,6 +493,20 @@ float grad(int hash, float x, float y, float z)
     float u = h < 8 ? x : y; // into 12 gradient vectors.
     float v = h < 4 ? y : h == 12 || h == 14 ? x : z;
     return ((h & 1) == 0 ? u : -u) + ((h & 2) == 0 ? v : -v);
+}
+
+float2 randomVector(float2 UV, float offset)
+{
+    float2x2 m = float2x2(15.27, 47.63, 99.41, 89.98);
+    UV = frac(sin(mul(UV, m)));
+    return float2(sin(UV.y * +offset) * 0.5 + 0.5, cos(UV.x * offset) * 0.5 + 0.5);
+}
+
+float3 randomVector(float3 UV, float offset)
+{
+    float3x3 m = float3x3(52.61, 24.96, 66.04, 4.22, 70.89, 26.07, 46.86, 40.52, 21.76);
+    UV = frac(sin(mul(UV, m)));
+    return float3(sin(UV.z * +offset) * 0.5 + 0.5, cos(UV.x * offset) * 0.5 + 0.5, sin(UV.y * +offset) * 0.5 + 0.5);
 }
 
 #endif
