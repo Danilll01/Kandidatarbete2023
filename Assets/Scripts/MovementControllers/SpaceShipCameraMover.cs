dc9a5832017d84e3c4e368f5bbc8d8cb65259c0b
Turbobus <59963570+Turbobus@users.noreply.github.com>
2023-04-28 18:33:41 +0200

    Player can now change camera position in spaceship

--- Assets/Scripts/MovementControllers/SpaceShipCameraMover.cs
+++ Assets/Scripts/MovementControllers/SpaceShipCameraMover.cs
@@ -4,16 +4,24 @@ using UnityEngine;
 
 public class SpaceShipCameraMover : MonoBehaviour
 {
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
+    [SerializeField] private Transform realCameraPosition;
+    [SerializeField] private Vector3[] cameraPositions;
+    private int currentCameraIndex = 0;
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!Input.GetKeyDown(KeyCode.V)) { return; }
+
+        if (Universe.player.boarded)
+        {
+            Debug.Log(currentCameraIndex);
+            Debug.Log(((cameraPositions.Length - 1) == currentCameraIndex));
+            Debug.Log(cameraPositions.Length - 1);
+            
+            currentCameraIndex = ((cameraPositions.Length - 1) == currentCameraIndex) ? 0 : currentCameraIndex += 1;
+            realCameraPosition.localPosition = cameraPositions[currentCameraIndex];
+        }
     }
 }
