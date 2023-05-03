f72150da7d831efb586bdaf3a4caf561595563c1
Daniel <perssondaniel01@gmail.com>
2023-05-03 23:13:21 +0200

    Added struct for handeling collections of foliage

---
+++ Assets/Scripts/Foliage/FoliageCollection.cs
@@ -0,0 +1,10 @@
+using System;
+using UnityEngine;
+
+[Serializable]
+public struct FoliageCollection
+{
+    public string name;
+    public GameObject[] gameObjects;
+    public BiomeRange biomeRange;
+}
