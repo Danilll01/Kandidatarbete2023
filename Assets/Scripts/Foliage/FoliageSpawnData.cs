ec295af0570a4fbeb04b4ae750b042494e57813e
Daniel <perssondaniel01@gmail.com>
2023-05-09 21:33:11 +0200

    Added biome name

--- Assets/Scripts/Foliage/FoliageSpawnData.cs
+++ Assets/Scripts/Foliage/FoliageSpawnData.cs
@@ -7,11 +7,13 @@ public class FoliageSpawnData
     public Vector3 position;
     public Quaternion rotation;
     public GameObject prefab;
+    public string biome;
 
-    public FoliageSpawnData(Vector3 position, Quaternion rotation, GameObject prefab)
+    public FoliageSpawnData(Vector3 position, Quaternion rotation, GameObject prefab, string biome)
     {
         this.position = position;
         this.rotation = rotation;
         this.prefab = prefab;
+        this.biome = biome;
     }
 }
