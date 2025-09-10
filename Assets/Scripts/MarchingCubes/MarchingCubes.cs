090a6aeca8d23dc88083aafe99a8be20245e3418
Turbobus <59963570+Turbobus@users.noreply.github.com>
2025-09-10 04:25:59 +0200

    Switched to WebGPU

--- Assets/Scripts/MarchingCubes/MarchingCubes.cs
+++ Assets/Scripts/MarchingCubes/MarchingCubes.cs
@@ -1,15 +1,7 @@
-using JetBrains.Annotations;
-using System;
-
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using Unity.Mathematics;
-using UnityEditor.Rendering;
 using UnityEngine;
 using UnityEngine.Rendering;
-using UnityEngine.UI;
+
 
 public class MarchingCubes
 {
